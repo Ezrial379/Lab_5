d642ca728b9b9408621fa3e73f34d181a72eb7fa
Ezrial379 <aaa503634@gmail.com>
2022-11-05 21:38:45 +0200

    Tasks

---
+++ Task_4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            var instance = MyFactory<MyClass>.FacrotyMethod();
+
+            Console.WriteLine(instance.GetType());
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+
+    public class MyFactory<T> where T : new()
+    {
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+
+    public class MyClass
+    {
+        public int Value { get; set; }
+        public MyClass(int value)
+
+        {
+
+            Value = value;
+
+        }
+
+        public MyClass() { }
+    }
+
+}
